30a44be605713c6ba80b74a4ea5237db5db599b2
Gabriel Escame <gabriel.escame@gmail.com>
2019-12-10 19:13:50 -0200

    PokeInfo-component-name-&-number

--- pokemonproject/src/Components/PokeInfo/index.jsx
+++ pokemonproject/src/Components/PokeInfo/index.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import api from '../../Services/api'
 
 const PokeInfo = (props) => {
   const [pokemonInfo, setPokemonInfo] = useState([])
 
   useEffect(() => {
-    const {name} = props.match.params;
+    const { name } = props.match.params;
     api.get(`pokemon/${name}`)
-     .then(Response => setPokemonInfo([Response.data]))
-     .catch(err => console.log(err))
+      .then(Response => setPokemonInfo([Response.data]))
+      .catch(err => console.log(err))
   }, [])
-  console.log(pokemonInfo);  
+  console.log(pokemonInfo);
   return (
-    <div>
-      {
-        pokemonInfo.map(e => <h1>{e.name}</h1>)
-      }
-    </div>
+    <>
+      <div>
+        {
+          pokemonInfo.map((e, idx) => (
+            <div key={idx} className="pokeInfo-container">
+              <h1>{`${e.name}|`} <span>{e.id <= 9 ? `00${e.id}`: null || e.id <= 99 ? `0${e.id}` : e.id}</span></h1>
+              <img src={e.id <= 9 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${e.id}.png` : null || e.id <= 99 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${e.id}.png` : `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${e.id}.png`} alt="poke-img"/>
+            </div>))
+        }
+      </div>
+      <Link to='/'>Back to pokédex</Link>
+    </>
   );
 }
 

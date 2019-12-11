b34f0152a2f13be4451bf100a2c6ae4a269e72c5
Gabriel Escame <gabriel.escame@gmail.com>
2019-12-10 22:35:39 -0200

    PokeInfo-component-pokemon-description

--- pokemonproject/src/Components/PokeInfo/index.jsx
+++ pokemonproject/src/Components/PokeInfo/index.jsx
@@ -3,24 +3,33 @@ import { Link } from 'react-router-dom'
 import api from '../../Services/api'
 
 const PokeInfo = (props) => {
-  const [pokemonInfo, setPokemonInfo] = useState([])
+  const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [pokemonDescription, setPokemonDescription] = useState([]);
 
   useEffect(() => {
     const { name } = props.match.params;
     api.get(`pokemon/${name}`)
-      .then(Response => setPokemonInfo([Response.data]))
+      .then(response => setPokemonInfo([response.data]))
+      .catch(err => console.log(err));
+    api.get(`https://pokeapi.co/api/v2/pokemon-species/${name}/`)
+      .then(response => setPokemonDescription([response.data]))
       .catch(err => console.log(err))
   }, [])
-  console.log(pokemonInfo);
+  console.log(pokemonDescription.map( e => e.flavor_text_entries.find(e => e.language.name === 'en')));
   return (
     <>
-      <div>
+      <div className="pokeInfo-container">
         {
           pokemonInfo.map((e, idx) => (
-            <div key={idx} className="pokeInfo-container">
-              <h1>{`${e.name}|`} <span>{e.id <= 9 ? `00${e.id}`: null || e.id <= 99 ? `0${e.id}` : e.id}</span></h1>
-              <img src={e.id <= 9 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${e.id}.png` : null || e.id <= 99 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${e.id}.png` : `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${e.id}.png`} alt="poke-img"/>
+            <div key={idx}>
+              <h1>{`${e.name}|`} <span>{e.id <= 9 ? `00${e.id}` : null || e.id <= 99 ? `0${e.id}` : e.id}</span></h1>
+              <img src={e.id <= 9 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${e.id}.png` : null || e.id <= 99 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${e.id}.png` : `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${e.id}.png`} alt="poke-img" />
             </div>))
+        }
+        {
+          pokemonDescription.map((e, idx) => (
+            <p>{e.flavor_text_entries.find(e => e.language.name === 'en').flavor_text}</p>
+          ))
         }
       </div>
       <Link to='/'>Back to pokédex</Link>

cfd5bb578982fb0ff899a6060e31c163178e644c
Gabriel Escame <gabriel.escame@gmail.com>
2019-12-11 03:52:44 -0200

    PokemonInfo-component-backgroundColor-changing

--- pokemonproject/src/Components/PokeInfo/index.jsx
+++ pokemonproject/src/Components/PokeInfo/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import './style.css'
 import api from '../../Services/api'
 
 const PokeInfo = (props) => {
@@ -15,24 +16,24 @@ const PokeInfo = (props) => {
       .then(response => setPokemonDescription([response.data]))
       .catch(err => console.log(err))
   }, [])
-  console.log(pokemonDescription.map( e => e.flavor_text_entries.find(e => e.language.name === 'en')));
+  console.log(pokemonInfo);
   return (
     <>
       <div className="pokeInfo-container">
         {
           pokemonInfo.map((e, idx) => (
-            <div key={idx}>
-              <h1>{`${e.name}|`} <span>{e.id <= 9 ? `00${e.id}` : null || e.id <= 99 ? `0${e.id}` : e.id}</span></h1>
-              <img src={e.id <= 9 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/00${e.id}.png` : null || e.id <= 99 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/0${e.id}.png` : `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${e.id}.png`} alt="poke-img" />
+            <div key={idx} className='pokeInfo-pokemon-header' style={e.types[e.types.length - 1].type.name === 'grass' ?{backgroundColor:'#9bcc50'} : {backgroundColor:'red'}}>
+              <h1 className='pokeInfo-pokemon-name'>{`${e.name}|`} <span className='pokeInfo-pokemon-number'>{e.id <= 9 ? `00${e.id}` : null || e.id <= 99 ? `0${e.id}` : e.id}</span></h1>
+              <img src={e.id <= 9 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${e.id}.png` : null || e.id <= 99 ? `https://assets.pokemon.com/assets/cms2/img/pokedex/full/0${e.id}.png` : `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${e.id}.png`} alt="poke-img" />
             </div>))
         }
         {
           pokemonDescription.map((e, idx) => (
-            <p>{e.flavor_text_entries.find(e => e.language.name === 'en').flavor_text}</p>
+            <p key={idx} className='pokeInfo-pokemon-description'>{e.flavor_text_entries.find(e => e.language.name === 'en').flavor_text}</p>
           ))
         }
-      </div>
       <Link to='/'>Back to pokédex</Link>
+      </div>
     </>
   );
 }
